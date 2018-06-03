03c41bfbea42f0ded411ec5f59f8534e84f5ee9c
AntonDemianenko18 <39765551+AntonDemianenko18@users.noreply.github.com>
2018-06-03 19:08:54 +0300

    Создание программы. Доработаный код

--- script.js
+++ script.js
@@ -165,3 +165,35 @@ $('#start').click(function(){
 	createCookie(name, value, 1);
 
 });
+
+function createCookie(name, value, days) {
+    if (days) 
+    {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 *1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else 
+    {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+        	var value = c.substring(nameEQ.length,c.length);
+            return value.split(",");
+        }
+    }
+    return null;
+}
